eac922cc5dc9a7263ea719e28f655690ea79ea65
Dean Quiñanola <deanq@Dean-Quinanolas-MacBook-Pro.local>
2013-11-19 15:16:50 -0800

    first pass at skinning

--- public/js/init.js
+++ public/js/init.js
@@ -4,7 +4,7 @@
 //var app = angular.module('AngularSFDemo', ['AngularForce', 'AngularForceObjectFactory', 'Contact', 'ui.bootstrap.dropdownToggle']);
 //app.constant('SFConfig', getSFConfig());
 
-var app = angular.module('AngularSFDemo', ['AngularForce', 'AngularForceObjectFactory', 'Contact', 'ui.bootstrap.dropdownToggle']);
+var app = angular.module('CustomerPath', ['AngularForce', 'AngularForceObjectFactory', 'Contact', 'ui.bootstrap.dropdownToggle']);
 var SFConfig = getSFConfig();
 
 SFConfig.maxListSize = 25;
