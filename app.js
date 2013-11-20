47bf7deb4cec6b6aa772c60201e2b8e0cce91d05
Harvey <ch4ch4@gmail.com>
2013-11-19 20:50:56 -0800

    added ping proxy

--- app.js
+++ app.js
@@ -68,6 +68,36 @@ app.all('/proxy/?*', function (req, res) {
     }).pipe(res);
 });
 
+app.all('/ping/*', function (req, res) {
+    log(req);
+    var body = req.body;
+    var contentType = "application/json";
+    var sfEndpoint = "https://na15.salesforce.com/services/apexrest/Ping/";
+    //if (body) {
+    //    //if doing oauth, then send body as form-urlencoded
+    //    if (sfEndpoint && sfEndpoint.indexOf('oauth2') > 0) {
+    //        body = getAsUriParameters(body);
+    //    } else {//for everything else, it's json
+    //        contentType = "application/json";
+            body = JSON.stringify(body);
+    //    }
+    //}
+
+    if ((!body || JSON.stringify(body) === "\"{}\"") && (typeof sfEndpoint != "string")) {
+        return res.send('Request successful (but nothing to proxy to SF)');
+    }
+    request({
+        url: sfEndpoint,// || "https://login.salesforce.com//services/oauth2/token",
+        method: req.method,
+        headers: {
+            "Content-Type": contentType,
+            "Authorization": req.headers["authorization"] || req.headers['x-authorization'],
+            "X-User-Agent": req.headers["x-user-agent"]
+        },
+        body: body
+    }).pipe(res);
+});
+
 function log(req) {
     console.log("req.headers[\"authorization\"] = " + req.headers["authorization"]);
     console.log("req.headers[\"x-authorization\"] = " + req.headers["x-authorization"]);
